a72f41f70aa5fce37ee2d50fa6d6872d808a3ec1
Minte-grace <mintenottsegaye23@gmail.com>
2020-08-09 18:12:15 +0300

    my commit

--- App.js
+++ App.js
@@ -1,40 +1,93 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { AppLoading } from 'expo';
-import * as Font from 'expo-font';
-import AppNavigator from './src/navigations/Navigator'
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { Image, StyleSheet, Text, View } from "react-native";
+import AppIntroSlider from "react-native-app-intro-slider";
+
+const slides = [
+  {
+    key: "one",
+    title: "JUST TRAVEL",
+    text:
+      "Lorem ipsum dolor sit amet consecte tuer adipsing elit sed diam monum my nibh eusimod eltor",
+    image: require("./images/1.png"),
+  },
+  {
+    key: "two",
+    title: "TAKE A BREAK",
+    text:
+      "Lorem ipsum dolor sit amet consecte tuer adipsing elit sed diam monum my nibh eusimod eltor",
+    image: require("./images/2.png"),
+  },
+  {
+    key: "three",
+    title: "ENJOY YOUR JOURNEY",
+    text:
+      "Lorem ipsum dolor sit amet consecte tuer adipsing elit sed diam monum my nibh eusimod eltor",
+    image: require("./images/3.png"),
+  },
+];
 
 export default class App extends React.Component {
-  state = {
-    fontIsLoaded : false
-  }
-
-  async componentDidMount(){
-    await Font.loadAsync({
-      'SemiBold' : require('./src/fonts/Montserrat-SemiBold.otf'),
-      'Bold' : require('./src/fonts/Montserrat-Bold.otf'),
-      'ExtraBold' : require('./src/fonts/Montserrat-ExtraBold.otf'),
-      'Medium' : require('./src/fonts/Montserrat-Medium.otf'),
-      'Regular' : require('./src/fonts/Montserrat-Regular.otf')
-    });
-    this.setState({fontIsLoaded:true})
-  }
-
-  render(){
+  state = { showHomePage: false };
+  _renderItem = ({ item }) => {
     return (
-     (this.state.fontIsLoaded === true) ? (<AppNavigator/>):(<AppLoading/>) 
-      
+      <View style={{ flex: 1 }}>
+        <Image
+          source={item.image}
+          style={{
+            resizeMode: "cover",
+            height: "73%",
+            width: "100%",
+          }}
+        />
+        <Text
+          style={{
+            paddingTop: 25,
+            paddingBottom: 10,
+            fontSize: 23,
+            fontWeight: "bold",
+            color: "#21465b",
+            alignSelf: "center",
+          }}
+        >
+          {item.title}
+        </Text>
+
+        <Text style={{
+          textAlign:"center",
+          color:"#b5b5b5",
+          fontSize:15,
+          paddingHorizontal:30
+        }}>
+          {item.text}
+        </Text>
+      </View>
+    );
+  };
+
+  render() {
+    if (this.state.showHomePage){
+      return <App/>
+    } else 
+    return (
+    <AppIntroSlider
+      renderItem={this._renderItem} 
+      data={slides} 
+      activeDotStyle={{
+        backgroundColor:"#21465b",
+        width:30
+      }}
+     />
+  
     );
   }
-  
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
