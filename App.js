989b0d5c63aad8a5e7fc869eeb8c450a7d329280
Mintesnot Tsegaye <46419626+Minte-grace@users.noreply.github.com>
2022-01-22 15:15:21 +0300

    R

--- App.js
+++ App.js
@@ -2,7 +2,7 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import AppNavigator from './src/navigations/Navigator'
-import * as Font from 'expo-font';
+import * as Font from 'expo-font';//
 import {AppLoading} from 'expo'
 
 export default class App extends React.Component{
